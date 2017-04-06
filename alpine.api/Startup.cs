e59bf6d7b5a40558331b6ba56f5d8c0630f9747a
joshharms <joshuaharms@me.com>
2017-04-05 20:58:43 -0700

    Initial Api JWT Validation Implementation
    Use temp dev signing key, issuer and audience

--- alpine.api/Startup.cs
+++ alpine.api/Startup.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -28,7 +28,10 @@ namespace alpine.api
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddMvc();
+			services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin()
+																	.AllowAnyMethod()
+																	 .AllowAnyHeader()));
+			services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,7 +40,42 @@ namespace alpine.api
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
+			app.UseCors("AllowAll");
+
+			ConfigureOAuth(app);
+
             app.UseMvc();
         }
+
+		public void ConfigureOAuth(IApplicationBuilder app)
+		{
+			var tokenValidationParameters = new TokenValidationParameters
+			{
+				// The signing key must match!
+				ValidateIssuerSigningKey = true,
+				IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("secretsecretsecretsecretkeyABC123!")),
+
+				// Validate the JWT Issuer (iss) claim
+				ValidateIssuer = true,
+				ValidIssuer = "ExampleIssuer",
+
+				// Validate the JWT Audience (aud) claim
+				ValidateAudience = true,
+				ValidAudience = "ExampleAudience",
+
+				// Validate the token expiry
+				ValidateLifetime = true,
+
+				// If you want to allow a certain amount of clock drift, set that here:
+				ClockSkew = TimeSpan.Zero
+			};
+
+			app.UseJwtBearerAuthentication(
+				new JwtBearerOptions
+				{
+					AutomaticAuthenticate = true,
+					AutomaticChallenge = true,
+				});
+		}
     }
 }
