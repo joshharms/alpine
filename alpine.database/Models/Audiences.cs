fecca90217bf9a44b6fcd22ee5648a62618b3190
joshharms <joshuaharms@me.com>
2017-04-08 18:20:27 -0700

    Initial check-in of alpine.database class library project

---
+++ alpine.database/Models/Audiences.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace alpine.database.Models
+{
+    public partial class Audiences
+    {
+        public Guid Id { get; set; }
+        public int ClusterId { get; set; }
+        public string Name { get; set; }
+        public string Base64Secret { get; set; }
+    }
+}
---
+++ alpine.database/Models/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace alpine.database.Models
+{
+    public partial class Users
+    {
+        public Guid Id { get; set; }
+        public int ClusterId { get; set; }
+        public bool Active { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime PasswordLastUpdated { get; set; }
+        public int AccessFailedCount { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public int RoleId { get; set; }
+        public int? CompanyId { get; set; }
+        public Guid? Avatar { get; set; }
+
+        public virtual Roles Role { get; set; }
+    }
+}
