cb66b4f29f53a6dce49537af03c304ee84162985
joshharms <joshuaharms@me.com>
2017-04-15 16:40:51 -0400

    Updated ValuesController to use new base DataController and implement repository pattern

--- alpine.api/Controllers/ValuesController.cs
+++ alpine.api/Controllers/ValuesController.cs
@@ -4,41 +4,51 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+using alpine.database.Models;
+using alpine.repository;
+
 namespace alpine.api.Controllers
 {
-    [Route("api/[controller]")]
-    public class ValuesController : Controller
-    {
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+     [Route("api/[controller]")]
+     public class ValuesController : DataController
+     {
+          private readonly IRepository<Users> _repository;
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+          public ValuesController(IRepository<Users> repository)
+		{
+               _repository = repository;
+		}
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
+		// GET api/values
+	     [HttpGet]
+          public IActionResult Get()
+		{
+               return SuccessMessage(_repository.GetAll());
+		}   
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+		//GET api/values/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+		  return "value";
+		}
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-    }
+		// POST api/values
+		[HttpPost]
+		public void Post([FromBody]string value)
+		{
+		}
+
+		// PUT api/values/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		// DELETE api/values/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+     }
 }
