10711588f57957c4ecb34aa7b46d4fa2aefc0711
joshharms <joshuaharms@me.com>
2017-04-15 16:40:17 -0400

    Created DataController as base controller to use for all web api controllers

---
+++ alpine.api/Controllers/DataController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using alpine.database.Models;
+
+namespace alpine.api.Controllers
+{
+     [Route("api/[controller]")]
+     public class DataController : Controller
+     {
+          protected JsonResult SuccessMessage( object data = null )
+          {
+               return Json( new
+               {
+                    time = DateTime.UtcNow,
+                    success = true,
+                    results = data
+               });
+          }
+     }
+}
