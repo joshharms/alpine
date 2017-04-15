00e902dcfeb0cd25c4634fe68c58f2c5e2658ae4
joshharms <joshuaharms@me.com>
2017-04-15 16:10:17 -0400

    Add repository and service projects

---
+++ alpine.repository/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace alpine.repository
+{
+     public interface IRepository<T>
+     {
+          T Get<TKey>(TKey id);
+          IQueryable<T> GetAll();
+          void Add(T entity);
+          void Update(T entity);
+          void Remove<TKey>(TKey id);
+     }
+}
---
+++ alpine.repository/Repositories/Repository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.EntityFrameworkCore;
+
+using alpine.database.Models;
+
+namespace alpine.repository
+{
+     public class Repository<T> : IRepository<T> where T : class
+     {
+          protected readonly alpineContext context;
+          protected DbSet<T> DbSet;
+
+          public Repository(alpineContext alpineContext)
+          {
+               context = alpineContext;
+               DbSet = context.Set<T>();
+          }
+
+          public void Add(T entity)
+          {
+               context.Set<T>().Add(entity);
+
+               Save();
+          }
+
+          public T Get<TKey>(TKey id)
+          {
+               return DbSet.Find(id);
+          }
+
+          public IQueryable<T> GetAll()
+          {
+               return DbSet;
+          }
+
+          public void Update(T entity)
+          {
+               Save();
+          }
+
+          private void Save()
+          {
+               context.SaveChanges();
+          }
+
+          public void Remove<TKey>(TKey id)
+          {
+               context.Remove(DbSet.Find(id));
+
+               Save();
+          }
+     }
+}
