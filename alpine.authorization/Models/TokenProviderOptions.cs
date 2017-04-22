332849a5e1187048030f3f6047306905ab8c76f4
joshharms <joshuaharms@me.com>
2017-04-22 19:06:59 -0400

    Change default JWT valid time to 5 min

--- alpine.authorization/Models/TokenProviderOptions.cs
+++ alpine.authorization/Models/TokenProviderOptions.cs
@@ -34,7 +34,7 @@ namespace alpine.authorization
 		/// The expiration time for the generated tokens.
 		/// </summary>
 		/// <remarks>The default is five minutes (300 seconds).</remarks>
-		public TimeSpan Expiration { get; set; } = TimeSpan.FromMinutes(1);
+		public TimeSpan Expiration { get; set; } = TimeSpan.FromMinutes(5);
 
 		/// <summary>
 		/// The signing key to use when generating tokens.
