c8d6ec51c14d59c0b903320b23565c108e18f0b6
joshharms <joshuaharms@me.com>
2017-04-22 19:08:52 -0400

    Add User controller to alpine.api

---
+++ alpine.api/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace alpine.api.Controllers
+{
+     [Route("api/[controller]")]
+     public class UserController : Controller
+     {
+          // POST api/user
+          [HttpPost]
+          public void Post([FromBody]string value)
+          {
+          }
+     }
+}
