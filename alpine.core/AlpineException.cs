c7a9f7be8b73aa04ba34c9c6e5d780b42016882f
joshharms <joshuaharms@me.com>
2017-06-11 01:06:51 -0400

    AlpineException created

---
+++ alpine.core/AlpineException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace alpine.core
+{
+    public class AlpineException : Exception
+    {
+        public AlpineException( string message )
+            : base( message )
+        {
+
+        }
+    }
+}
