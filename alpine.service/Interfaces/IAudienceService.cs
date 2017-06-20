1184cdf0d114afb0bdde10e13e6fa658b91ba95c
joshharms <joshuaharms@me.com>
2017-06-20 02:34:17 -0400

    Added AudienceService

---
+++ alpine.service/Interfaces/IAudienceService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using alpine.database.Models;
+
+namespace alpine.service.Interfaces
+{
+    public interface IAudienceService
+    {
+        Audiences AddAudience( string name );
+        Audiences GetAudience( Guid id );
+    }
+}
---
+++ alpine.service/Services/AudienceService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+using alpine.database.Models;
+using alpine.service.Interfaces;
+
+namespace alpine.service.Services
+{
+    public class AudienceService : BaseService, IAudienceService
+    {
+        public AudienceService( alpineContext ctx ) : base( ctx )
+        { }
+
+        public Audiences AddAudience( string name )
+        {
+            var audienceId = Guid.NewGuid();
+
+            var key = new byte[ 32 ];
+            RandomNumberGenerator.Create().GetBytes( key );
+
+            var base64Secret = Convert.ToBase64String( key );
+
+            Audiences a = new Audiences
+            {
+                Id = audienceId,
+                Name = name,
+                Base64Secret = base64Secret
+            };
+
+            context.Audiences.Add( a );
+            context.SaveChanges();
+
+            return a;
+        }
+
+        public Audiences GetAudience( Guid id )
+        {
+            var audience = context.Audiences.SingleOrDefault( x => x.Id == id );
+
+            if( null != audience )
+            {
+                return audience;
+            }
+
+            return null;
+        }
+    }
+}
