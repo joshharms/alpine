2106d9d0c20b9d673146839bbb693e13886f1277
joshharms <joshuaharms@me.com>
2017-06-20 02:35:32 -0400

    Added Audience Web API controller

---
+++ alpine.authorization/Controllers/AudienceController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+
+using alpine.database.Models;
+using alpine.service.Interfaces;
+
+namespace alpine.authorization.Controllers
+{
+    [Route( "api/[controller]" )]
+    public class AudienceController : Controller
+    {
+        private readonly IAudienceService _audienceService;
+
+        public AudienceController( IAudienceService audienceService )
+        {
+            _audienceService = audienceService;
+        }
+
+        // GET api/audience/5
+        [HttpGet( "{id}" )]
+        public Audiences Get( Guid id )
+        {
+            return _audienceService.GetAudience( id );
+        }
+
+        // POST api/audience
+        [HttpPost]
+        public Audiences Post( string name )
+        {
+            return _audienceService.AddAudience( name );
+        }
+    }
+}
