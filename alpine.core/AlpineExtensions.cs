cb6fcee15c999ca0df607a09c31848a2be2ce782
joshharms <joshuaharms@me.com>
2017-09-30 18:52:50 -0400

    Add Utc string extension method

---
+++ alpine.core/AlpineExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace alpine.core
+{
+    public static class AlpineExtensions
+    {
+        public static string ToUtcString( this DateTime val )
+        {
+            if ( null != val )
+            {
+                return val.ToString( "yyyy-MM-ddTHH:mm:ssZ" );
+            }
+
+            return null;
+        }
+    }
+}
