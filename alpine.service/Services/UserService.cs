f5e91344f5b739a4e79c319628ec839a900c646d
joshharms <joshuaharms@me.com>
2017-09-30 18:53:11 -0400

    Get Users method in UserService

--- alpine.service/Services/UserService.cs
+++ alpine.service/Services/UserService.cs
@@ -11,12 +11,37 @@ namespace alpine.service.Services
 {
     public class UserService : BaseService, IUserService
     {
+        private List<UserSummary> _users;
+
         public UserService( alpineContext ctx, AuthenticationTokenAccessor auth ) : base( ctx, auth )
-        { }
+        {
+            _users = new List<UserSummary>();
+        }
 
         public List<UserSummary> GetUsers()
         {
-            throw new NotImplementedException();
+            List<Users> users = new List<Users>();
+
+            if ( token.role == AlpineRoles.RoleType.PlatformAdministrator.ToDisplayString() )
+            {
+                users = db.Users.ToList();
+            }
+
+            foreach ( var user in users )
+            {
+                UserSummary u = new UserSummary();
+                u.id = user.Id;
+                u.active = user.Active;
+                u.email = user.Email;
+                u.phoneNumber = user.PhoneNumber;
+                //u.avatar = user.Avatar;
+                u.createdDate = user.CreatedDate.ToUtcString();
+
+                _users.Add( u );
+            }
+
+            return _users;
+
         }
     }
 }
