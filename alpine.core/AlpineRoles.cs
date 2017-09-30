0ce53490b72b5e335d392a1fb3861bbfb4eba48f
joshharms <joshuaharms@me.com>
2017-09-30 18:52:37 -0400

    Change base service db variable, add roles

---
+++ alpine.core/AlpineRoles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace alpine.core
+{
+    public static class AlpineRoles
+    {
+        public enum RoleType
+        {
+            PlatformAdministrator = 1,
+            Administrator = 2,
+        }
+
+        public static string ToDisplayString( this RoleType t )
+        {
+            switch ( t )
+            {
+                case RoleType.PlatformAdministrator:
+                    return "Platform Administrator";
+                case RoleType.Administrator:
+                    return "Administrator";
+            }
+
+            return "";
+        }
+    }
+}
--- alpine.service/Services/BaseService.cs
+++ alpine.service/Services/BaseService.cs
@@ -9,23 +9,26 @@ namespace alpine.service
 {
     public class BaseService : IDisposable
     {
-        public readonly alpineContext context;
+        public readonly alpineContext db;
         public readonly AuthenticationToken token;
+
+        protected string PlatformAdminDisplay = AlpineRoles.RoleType.PlatformAdministrator.ToDisplayString();
+        protected string AdministratorDisplay = AlpineRoles.RoleType.Administrator.ToDisplayString();
 
         public BaseService( alpineContext alpineContext )
         {
-            context = alpineContext;
+            db = alpineContext;
         }
 
         public BaseService( alpineContext alpineContext, AuthenticationTokenAccessor authenticationTokenAccessor )
         {
-            context = alpineContext;
+            db = alpineContext;
             token = authenticationTokenAccessor.token;
         }
 
         public void Dispose()
         {
-            context.Dispose();
+            db.Dispose();
         }
     }
 }
